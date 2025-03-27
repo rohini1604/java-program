01759c904e0209df95fa986633dd6acfc2fac123
rohini1604 <rohinibhagya12@gmail.com>
2025-03-27 20:03:01 +0530

    wow

---
+++ Student2.java
@@ -0,0 +1,21 @@
+package rohini123;
+
+public class Student2 {
+      static int id=441;
+      String name="Rohini";
+      
+	public static void main(String[] args) {
+		
+	
+		System.out.println(id);
+		System.out.println(new Student2().name);
+		int height=50;
+		System.out.println(height);
+	}
+	public static void test() {
+		System.out.println(id);
+	}
+
+	}
+
+
--- student.java
+++ student.java
@@ -1,38 +1,13 @@
-package encapsulation;
+package rohini123;
 
-public class student {
-	private int rollnumber;
-	private String name;
-	boolean isAttended;
-	
-	public student(int rollnumber) {
-		this.rollnumber=rollnumber;
-	}
-    
-	public void setstudentAttendence(boolean flag) {
-		if(! isAttended)
-		isAttended=flag;
-		System.out.println("teacher assigned  attedence to student");
-	}
-	
-	public int getRollnumber() {
-		return rollnumber;
+public class Student {
+	int id=441;
+
+	public static void main(String[] args) {
+		Student student=new Student();
+		System.out.println(student.id);
+		
+
 	}
 
-	public void setRollnumber(int rollnumber) {
-		this.rollnumber = rollnumber;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public boolean getstudentAttendence() {
-		System.out.println("teacher accessed attedence to student");
-		return isAttended;
-	}
 }
