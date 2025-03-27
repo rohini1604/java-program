5e4435169bab55755d3dd840cae58bec3b78fb7f
rohini1604 <rohinibhagya12@gmail.com>
2025-03-27 08:00:23 +0530

    sis

---
+++ OneDimensionalArray.java
@@ -0,0 +1,19 @@
+package myfirstjava;
+
+public class OneDimensionalArray {
+
+	public static void main(String[] args) {
+		int [] a=new int[3];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		System.out.println("One Dimensional Array");
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+
+		
+
+	}
+
+}
---
+++ TwoDimansionalArray.java
@@ -0,0 +1,21 @@
+package myfirstjava;
+
+public class TwoDimansionalArray {
+
+	public static void main(String[] args) 
+	{
+		int a[][]= {{1,2,3},{3,4,5},{6,7,8},{4,5,6}};
+		for(int i=0;i<4;i++)
+			{
+				for(int j=0;j<3;j++)
+				{
+					System.out.print(a[i][j]+" ");
+				}
+				System.out.println();
+				}
+			}
+		}
+
+	
+
+
