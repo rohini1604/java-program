baee2cf116f3331d8dc88ffb2459a6d16c91750e
rohini1604 <rohinibhagya12@gmail.com>
2025-03-27 16:26:08 +0530

    hlo

---
+++ Dog.java
@@ -0,0 +1,15 @@
+package rohini123;
+
+public class Dog {
+     public String breed="German shepard";
+     public int height=30;
+     public int weight=35;
+     
+	public static void main(String[] args) {
+		System.out.println("test");
+		Dog dog=new Dog();
+		System.out.println("dog breed");
+
+	}
+
+}
