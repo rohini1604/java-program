60a7ec46b9f26b09710dd9f00232356105bc45ad
rohini1604 <rohinibhagya12@gmail.com>
2025-03-27 19:30:22 +0530

    wow

---
+++ variabledefine.java
@@ -0,0 +1,12 @@
+package rohini123;
+
+public class variabledefine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pincode=50050;
+		System.out.println(pincode);
+//data type variableName=variableValue;
+	}
+
+}
---
+++ variablereassigning.java
@@ -0,0 +1,17 @@
+package rohini123;
+
+public class variablereassigning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int pincode;
+		System.out.println(441);
+		pincode=50050;
+		pincode=50020;
+		System.out.println(pincode);
+		//define & initialisation 
+		
+
+	}
+
+}
