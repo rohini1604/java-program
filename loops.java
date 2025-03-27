173fbf4b4c16bde6c261b96468563161da20d7c3
rohini1604 <rohinibhagya12@gmail.com>
2025-03-27 07:57:32 +0530

    amma

---
+++ loops.java
@@ -0,0 +1,16 @@
+package myfirstjava;
+
+public class loops {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=50;i++) {
+			for(int j=1;j<=50;j++) {
+			
+		
+		System.out.println(i*j+"/t");
+	}
+			System.out.println();
+		}
+	}
+		}
